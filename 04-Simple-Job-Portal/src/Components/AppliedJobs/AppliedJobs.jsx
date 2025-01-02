3510484ceef77bf8bf4347fc0d024e1440774c6d
rubaiiyat <abirr1516@gmail.com>
2025-01-02 21:06:56 +0600

    filter jobs

--- 04-Simple-Job-Portal/src/Components/AppliedJobs/AppliedJobs.jsx
+++ 04-Simple-Job-Portal/src/Components/AppliedJobs/AppliedJobs.jsx
@@ -7,6 +7,7 @@ const AppliedJobs = () => {
   const jobs = useLoaderData();
 
   const [jobApplieds, setJobApplied] = useState([]);
+  const [displayJobs, setDisplayJobs] = useState([]);
   useEffect(() => {
     const storedJobsId = getStoredApplication();
 
@@ -22,24 +23,47 @@ const AppliedJobs = () => {
       }
 
       setJobApplied(appliedJobs);
-
+      setDisplayJobs(jobApplieds);
       console.log(jobs, storedJobsId, appliedJobs);
     }
   }, []);
+
+  const handleJobFilter = (filter) => {
+    if (filter == "all") {
+      setDisplayJobs(jobApplieds);
+    } else if (filter === "remote") {
+      const remoteJobs = jobApplieds.filter((job) => job.job_type === "Remote");
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsite = jobApplieds.filter((job) => job.job_type === "On-Site");
+      setDisplayJobs(onsite);
+    }
+  };
   return (
     <div className="flex flex-col items-center justify-center h-auto  p-6">
-      <select className="select select-bordered w-full max-w-xs">
-        <option disabled selected>
-          Job Type
-        </option>
-        <option>All</option>
-        <option>Remote</option>
-        <option>On-Site</option>
-      </select>
+      <div className="dropdown">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Sort By
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+        >
+          <li onClick={() => handleJobFilter("all")}>
+            <a> All</a>
+          </li>
+          <li onClick={() => handleJobFilter("remote")}>
+            <a>Remote</a>
+          </li>
+          <li onClick={() => handleJobFilter("onsite")}>
+            <a>On-Site</a>
+          </li>
+        </ul>
+      </div>
       <h1>This is applied jobs pages</h1>
       <h1>{jobApplieds.length}</h1>
 
-      {jobApplieds.map((job) => (
+      {displayJobs.map((job) => (
         <AppliedJob key={job.id} job={job}></AppliedJob>
       ))}
     </div>
